56ee51d98a84b606986625fd1fb1a0420383d498
mambogamer13 <gagss1@iscte-iul.pt>
2026-09-25 15:48:36 +0100

    criar docs

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"iscteiul.ista.battleship","l":"Position"},{"p":"iscteiul.ista.battleship","l":"Ship"},{"p":"iscteiul.ista.battleship","l":"Tasks"}];updateSearchResults();
